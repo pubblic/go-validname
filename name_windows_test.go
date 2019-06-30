5310f986e49359302740960d8a1de76137644142
pubblic <pubblic256@gmail.com>
2019-07-01 00:22:57 +0900

    refactor testing

--- name_windows_test.go
+++ name_windows_test.go
@@ -4,30 +4,30 @@ import (
 	"testing"
 )
 
-var tc = []struct {
-	name        string
-	regularName string
-	regularized bool
-}{
-	{"", "", true},
-	{"abc ", "abc", true},
-	{"한글", "한글", true},
-	{"a/b/c", "a／b／c", true},
-	{".?.CON|", ".？.CON｜", true},
-	{".PRN", ".PRN", true},
-	{"PRN...  ", "PRN...  ", false},
-}
-
 func TestRegularName(t *testing.T) {
-	for _, args := range tc {
-		regularName, regularized := RegularName(args.name)
-		if regularized != args.regularized {
-			t.Errorf("%q is not correctly regularized", args.name)
-			continue
+	tests := []struct {
+		name    string
+		regular string
+		can     bool
+	}{
+		{"", "", true},
+		{"abc ", "abc", true},
+		{"한글", "한글", true},
+		{"a/b/c", "a／b／c", true},
+		{".?.CON|", ".？.CON｜", true},
+		{".PRN", ".PRN", true},
+		{"PRN...  ", "PRN...  ", false},
+	}
+
+	for _, test := range tests {
+		regular, can := RegularName(test.name)
+		if test.regular != regular {
+			t.Errorf("RegularName(%q) is expected to return %q, not %q",
+				test.name, test.regular, regular)
 		}
-		if regularName != args.regularName {
-			t.Errorf("expect %q, but actually got %q", args.regularName, regularName)
-			continue
+		if test.can != can {
+			t.Errorf("the name %q cannot be regularized",
+				test.name)
 		}
 	}
 }
